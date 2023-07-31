571f9a5d2bebe9acaf401e15cef2022b220c520a
ElliotRipa <elliot.ripa@gmail.com>
2023-07-31 19:14:05 +0200

    Add logic.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        LineReader lr = new LineReader();
+
+        long startTime = System.nanoTime();
+        ArrayList<String> strings = lr.readLines("/H:\\Wiki Dumps\\en Wikipedia 2023-07-20\\enwiki-20230720-pages-articles-multistream.xml", 10000000);
+
+        long stopTime = System.nanoTime();
+
+        long timeDiff = stopTime - startTime;
+
+        System.out.println("It took " + timeDiff / 1000000 + " milliseconds");
+
         System.out.println("Hello world!");
     }
 }
