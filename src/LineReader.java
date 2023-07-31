5d6ce67451959b8ac3f945e6ae7a9d08b6afe0fd
ElliotRipa <elliot.ripa@gmail.com>
2023-07-31 19:13:50 +0200

    Add LineReader.java

--- src/LineReader.java
+++ src/LineReader.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class LineReader {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
+public class LineReader {
+
+    public ArrayList<String> readLines(String path, int offset) {
+        ArrayList<String> output = new ArrayList<>();
+        File file = new File(path);
+
+        try (BufferedReader br = Files.newBufferedReader(file.toPath())) {
+            for (int i = 0; i < offset; i++) {
+                br.readLine();
+            }
+
+            for(int j = 0 ; j < 10000000 ; j++) {
+                output.add(br.readLine());
+            }
+
+            return output;
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
